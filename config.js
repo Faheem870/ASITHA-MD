5a6ec22f1dd3f07b16fbd21c78d1cab9b6b947ce
M FAHEEM <oyit8t@gmail.com>
2024-10-08 18:10:23 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=vvg3mApQ#oannE0Jt6xQC-N5oP0hv9xvXZcvXFm2-ce31oqnC56s",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
